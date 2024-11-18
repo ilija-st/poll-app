e6ff2c2cb140fbc83c83d87048ae0ef18437cf99
Ilija Stojanovic <ilija.stojanovic@nutanix.com>
2024-11-18 15:46:12 +0100

    Implement generating jwt token pairs and add default values for JWT token

---
+++ backend/cmd/api/auth.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type Auth struct {
+	// Who is issueing tokens, company.com
+	Issuer string
+	// Who should be able to use tokens
+	Audience string
+	// Secret for signing tokens
+	Secret string
+	// How long should the token last
+	TokenExpiry time.Duration
+	// Less information, used to reauthn the user
+	RefreshExpiry time.Duration
+	CookieDomain  string
+	CookiePath    string
+	CookieName    string
+}
+
+type jwtUser struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+type TokenPairs struct {
+	Token        string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type Claims struct {
+	jwt.RegisteredClaims
+}
+
+func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs, error) {
+	// Create a token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// Set the claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	claims["sub"] = fmt.Sprint(user.ID)
+	claims["aud"] = j.Audience
+	claims["issuer"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix()
+	claims["typ"] = "JWT"
+
+	// Set the expiry
+	claims["exp"] = time.Now().UTC().Add(j.TokenExpiry).Unix()
+
+	// Create a signed token
+	signedAccessToken, err := token.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// Create a refresh token
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	refreshTokenClaims := refreshToken.Claims.(jwt.MapClaims)
+	refreshTokenClaims["sub"] = fmt.Sprint(user.ID)
+	refreshTokenClaims["iat"] = time.Now().UTC().Unix()
+
+	// Set expiry for refresh token
+	refreshTokenClaims["exp"] = time.Now().UTC().Add(j.RefreshExpiry).Unix()
+
+	// Create signed refresh token
+	signedRefreshToken, err := refreshToken.SignedString([]byte(j.Secret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// Create TokenPairs and populate with signed tokens
+	var tokenPairs = TokenPairs{
+		Token:        signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+
+	// Return TokenPairs
+	return tokenPairs, nil
+}
--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"backend/internal/repository"
 	"backend/internal/repository/dbrepo"
 	"flag"
+	"time"
 
 	_ "github.com/lib/pq"
 
@@ -15,19 +16,33 @@ import (
 const port = 8080
 
 type application struct {
-	DSN    string
-	Domain string
-	DB     repository.DatabaseRepo
+	DSN          string
+	Domain       string
+	DB           repository.DatabaseRepo
+	auth         Auth
+	JWTSecret    string
+	JWTIssuer    string
+	JWTAudience  string
+	CookieDomain string
 }
 
 func main() {
+	// set application config - type that stores info that app needs (ex. for database, or jwt secret, etc.)
 	var app application
 
+	// read from command line - we might pass flag that says this api is only for domain example.com
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=polls sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "poll.com", "signing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience", "poll.com", "signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
+	flag.StringVar(&app.Domain, "domain", "poll.com", "domain")
+	flag.Parse()
 
 	app.Domain = "poll.com"
 	log.Println("Starting application on port", port)
 
+	// connect to the database
 	conn, err := app.connectToDB()
 	if err != nil {
 		log.Fatal(err)
@@ -38,6 +53,17 @@ func main() {
 	err = app.DB.SeedData()
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	app.auth = Auth{
+		Issuer:        app.JWTIssuer,
+		Audience:      app.JWTAudience,
+		Secret:        app.JWTSecret,
+		TokenExpiry:   time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath:    "/",
+		CookieName:    "Host-refresh_token",
+		CookieDomain:  app.CookieDomain,
 	}
 
 	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
