c1fc7738ed8c1e190caf7fa48c3411268145b061
Ilija Stojanovic <ilija.stojanovic@nutanix.com>
2024-11-19 10:50:12 +0100

    Implement routes and App component

--- frontend/poll-app/src/App.js
+++ frontend/poll-app/src/App.js
@@ -1,8 +1,92 @@
+import { useCallback, useEffect, useState } from "react";
+import Navbar from "./components/navbar/Navbar";
+import AppTheme from "./components/shared-theme/AppTheme";
+import { CssBaseline } from "@mui/material";
+import Container from "@mui/material/Container";
+import { Outlet } from "react-router-dom";
+
 function App() {
+  const [jwtToken, setJwtToken] = useState("");
+  const [tickInterval, setTickInterval] = useState();
+
+  const toggleRefresh = useCallback(
+    (status) => {
+      console.log("clicked");
+
+      if (status) {
+        console.log("turning on ticking");
+        let i = setInterval(() => {
+          const requestOptions = {
+            method: "GET",
+            credentials: "include",
+          };
+
+          fetch(`/refresh`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.access_token) {
+                setJwtToken(data.access_token);
+              }
+            })
+            .catch((error) => {
+              console.log("user is not logged in");
+            });
+        }, 600000);
+        setTickInterval(i);
+        console.log("setting tick interval to", i);
+      } else {
+        console.log("turning off ticking");
+        console.log("turning off tickInterval", tickInterval);
+        setTickInterval(null);
+        clearInterval(tickInterval);
+      }
+    },
+    [tickInterval]
+  );
+
+  useEffect(() => {
+    if (jwtToken === "") {
+      const requestOptions = {
+        method: "GET",
+        credentials: "include",
+      };
+
+      fetch(`/refresh`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.access_token) {
+            setJwtToken(data.access_token);
+            toggleRefresh(true);
+          }
+        })
+        .catch((error) => {
+          console.log("user is not logged in", error);
+        });
+    }
+  }, [jwtToken, toggleRefresh]);
+
   return (
-    <div className="container">
-      <h2>Poll App</h2>
-    </div>
+    <AppTheme>
+      <CssBaseline enableColorScheme />
+      <Container
+        maxWidth="lg"
+        component="main"
+        sx={{ display: "flex", flexDirection: "column", my: 16, gap: 4 }}
+      >
+        <Navbar
+          jwtToken={jwtToken}
+          updateJwtToken={(val) => setJwtToken(val)}
+          toggleRefresh={() => toggleRefresh()}
+        />
+        <Outlet
+          context={{
+            jwtToken,
+            setJwtToken,
+            toggleRefresh,
+          }}
+        />
+      </Container>
+    </AppTheme>
   );
 }
 
--- frontend/poll-app/src/index.js
+++ frontend/poll-app/src/index.js
@@ -1,10 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import SignIn from "./components/sign-in/SignIn";
+import SignUp from "./components/sign-up/SignUp";
+import ErrorPage from "./components/error-page/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Poll from "./components/poll/Poll";
+import Home from "./components/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/polls",
+        element: <Poll />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
